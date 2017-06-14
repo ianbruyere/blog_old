67499708c98062be59db8fbfadb432cc916d1058
Ian Bruyere <Ian Bruyere>
2017-06-14 17:06:43 -0500

    whoops forgot to config my new module

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault(
         "DJANGO_SETTINGS_MODULE",
-        "personalWebsite.settings"
+        "personalWebsite.productionSettings"
     )
 
     from django.core.management import execute_from_command_line
